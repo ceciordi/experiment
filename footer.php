85a08f81434788ed663566256c373fb0213f9f10
cecilia romero <1ceciliaromero@gmail.com>
2017-07-04 00:18:46 -0400

    progress on eloy's recommendations.

---
+++ footer.php
@@ -0,0 +1,6 @@
+	<nav class="page-footer navbar navbar-default navbar-fixed-bottom">
+			<div class="container">
+				<span>&copy;Mänhsturs 2017</span>
+				<span>Brooklyn, NY</span>
+			</div>
+		</nav>
---
+++ navbar.php
@@ -0,0 +1,16 @@
+ <div class="page-header">
+			<div class="container">
+				<div class="row">
+					<div class="title pull-left">
+						<a href="#">
+							<h1>Mänhsturs</h1>
+						</a>
+					</div>
+					<div class="controls pull-right">
+						<button class="btn btn-default">OPTIONS</button>
+						<button class="btn btn-default">OPTIONS</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	
