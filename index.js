33c31d2230e9533d475173d09330b9642fd862e5
cecilia romero <1ceciliaromero@gmail.com>
2017-07-27 13:45:41 -0400

    previous updates to css and index

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 window.addEventListener('load', function () {
-	var slice = Array.prototype.slice,
-		cards = slice.call(document.querySelectorAll('.cards > .card'), 0);
-	cards.forEach(function (card) {
+	var cards = document.querySelectorAll('.cards > .card');
+	for (i = 0; i < cards.length; i += 1) {
+		var card = cards.item(i);
 		card.addEventListener('click', function (e) {
 			e.currentTarget.classList.toggle('active');
 		});
-	});
+	}
 });
