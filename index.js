dcfb0da71c6f3e224b1bd9eb8d124142ad61e53e
cecilia romero <1ceciliaromero@gmail.com>
2017-07-27 14:03:42 -0400

    progress on play.html

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', function () {
 	var cards = document.querySelectorAll('.cards > .card');
-	for (i = 0; i < cards.length; i += 1) {
+	for (var i = 0; i < cards.length; i += 1) {
 		var card = cards.item(i);
 		card.addEventListener('click', function (e) {
 			e.currentTarget.classList.toggle('active');
