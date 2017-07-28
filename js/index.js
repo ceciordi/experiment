c96d02733f4a74774a653189f2dfbb13467da4db
cecilia romero <1ceciliaromero@gmail.com>
2017-07-28 19:15:57 -0400

    progress on setting up monsters intro

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 $(function () {
+
+    'use strict';
+
 	var $body = $('body'),
 		$fauxHeader = $('.page-header.faux-header'),
+		$monstersIntroView = $('#monsters-intro'),
+		$monstersIntroContainer = $('.monsters-for-intro', $monstersIntroView),
 		$cards = $('.cards > .card');
 		$cards.on('click', function (e) {
 			e.currentTarget.classList.toggle('active');
@@ -19,14 +24,55 @@ $(function () {
 			}
         });
 
-        function initMonstersIntro ($monstersIntro) {
-        	$monstersIntro.find('.monster').each(function (ind, element) {
-        		element
-				// Random x and y based on stage dimensions
-				// Random distance based on stage height
-				// Scale based on random distance
-				// Set to animate
+	function initMonstersIntro ($monstersParentContainer) {
+
+		// Get all monster divs
+		$('> div', $monstersParentContainer).each(function (ind, element) {
+
+			// Initial values needed for position images randomly
+			var parentContainerWidth = $monstersParentContainer.width(),
+				parentContainerHeight = $monstersParentContainer.height(),
+				imgWidth = 377,
+				imgHeight = 365,
+
+				// Variables for animation
+				distance = Math.random() * 200 + 1,
+				speed = 20 / distance,
+				initialScale = 1000 / distance * 0.01,
+				scale = initialScale > 1 ? 1 : initialScale,
+
+				// Random x and y based on parent container width and height
+				x = Math.random() * parentContainerWidth - (imgWidth * scale / 2),
+				y = Math.random() * parentContainerHeight - (imgHeight * scale / 2),
+
+				$elm = $(element);
+
+			console.log('z-index', distance * -1, 'scale', scale, 'x', x, 'y', y);
+
+			// Store animation values using jQuery.data()
+			$elm.data({
+				monsterScale: scale,
+				monsterSpeed: speed,
+				monsterDistance: distance,
+				monsterX: x,
+				monsterY: y,
 			});
-		}
+
+			// Randomly position our monster in preparation for our animation
+			$elm.css({
+				left: x,
+				top: y,
+                transform: `scale(${scale}, ${scale})`,
+                zIndex: Math.round(distance * -1)
+            });
+
+			// Set to animate
+            //translate(${x}px, ${y}px)
+        });
+	}
+
+
+
+    initMonstersIntro($monstersIntroContainer);
 
 });
